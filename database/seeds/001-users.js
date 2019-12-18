9941a232ed86f12e236945b31bb9426f88751ddc
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-18 17:40:01 -0500

    folders setup and MVP hit

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,13 +1,9 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+	// Deletes ALL existing entries
+	// Inserts seed entries
+	return knex('users').insert([
+		{ username: 'Penni', password: 'Penni', department: 'doggos' },
+		{ username: 'Buddha', password: 'Buddha', department: 'doggos' },
+		{ username: 'Ryan', password: 'Ryan', department: 'hoomans' },
+	]);
 };
